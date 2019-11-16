c881e8c762ab59fa892ec242099228f2ec06e752
Erin Johnson <erinjohnson47@gmail.com>
2019-11-16 13:15:08 -0800

    api call to umg - not working, commit to pull from master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-// import Search from './components/Search';
+import Search from './components/Search';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Search/>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,30 @@ class Search extends Component {
     query: '',
   }
 
+  componentDidMount() {
+    this.getGenres
+  }
+
+  getGenres = async () => {
+    try {
+      const genres = await fetch(`https://hackathon.umusic.com/prod/v1/tracks`, {
+        schema: loadJSON(__dirname + "/streaming.json"),
+        // baseURL: 'https://hackathon.umusic.com/prod',
+        extraHeaders: {
+          'x-api-key': process.env.UMG_API_KEY
+        },
+        type: 'text'
+      })
+      const genresJSON = await genres.json();
+      this.setState({query: genresJSON}, () => {
+        console.log(this.state.query)
+      })
+      
+    } catch (error) {
+      console.log(error, "error in catch")
+    }
+  } 
+
   handleInputChange = () => {
     this.setState({
       query: this.search.value
