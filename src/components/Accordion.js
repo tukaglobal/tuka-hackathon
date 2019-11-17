b10a08a42194773843e01754d09f37d587686afb
Joe Domovitch <jdomovitch@gmail.com>
2019-11-16 17:02:02 -0800

    icon rotate transition

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -16,9 +16,7 @@ class Accordion extends Component {
     return (
       <div className={this.state.active ? "accordion accordion--expand" : "accordion"}>
         <button className="accordion__button" onClick={this.toggleClass}>
-          {/* <span className="accordion__button--icon">►</span> */}
-          {/* <i className="fas fa-angle-right"></i> */}
-          <img src="../../assets/arrow-right.png" alt="arrow right button" className/>
+          <img src="../../assets/arrow-right.png" alt="arrow right button" className={this.state.active ? "accordion__icon accordion__icon--rotate" : "accordion__icon"}/>
         </button>
       </div>
     )
